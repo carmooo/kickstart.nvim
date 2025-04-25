a92c0c78c888cb858c160fab791fd6e3f1f0d995
carmooo <joaorvcarmo@gmail.com>
2025-04-25 10:43:35 +0100

    add auto imports for pyright

--- init.lua
+++ init.lua
@@ -667,7 +667,17 @@ require('lazy').setup({
       local servers = {
         clangd = {},
         gopls = {},
-        pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoImportCompletions = true,
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
